ee6bc14cf382b63a68359704220daabc19649547
wick3dr0se <wick3dr0se@protonmail.com>
2022-06-14 18:56:13 -0500

    inital

---
+++ phrases.sh
@@ -0,0 +1,7 @@
+phrase=(
+'pigs can not fly but elephants can'
+'he carried the bananas in a cardboard box on his head'
+'suddenly the room filled with a deafening silence'
+'she let out a silent scream as the cat walked throug the door carrying a dead bird'
+"you are clearly confused by the situation you've found yourself in"
+)
