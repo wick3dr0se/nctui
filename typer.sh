0ff022dfe7ca9f86b63e592bdf612d27a73e0c85
wick3dr0se <wick3dr0se@protonmail.com>
2022-06-14 22:32:50 -0500

    minor output change; rmv handling .

--- typer.sh
+++ typer.sh
@@ -3,28 +3,30 @@
 . ./phrases.sh
 phrase_count=${#phrase[@]}
 phrase=${phrase[`echo $((RANDOM%phrase_count))`]}
-printf '\t\e[40;1;37m%s\e[0m\n\e[1m%s\e[0m\n' \
+printf '\t\e[47;30m%s\e[0m\n\n\e[1m%s\e[0m\n' \
 	"Typer - Type the phrase below as quick as possible" "$phrase"
 
 timeStart=`date +%s`
 
 while read -reN1 typed_char ; do
-	echo
-	typed_phrase+=$typed_char
-	if [[ $phrase =~ ^$typed_phrase ]] ; then
-		characters=${#typed_phrase}
-		printf '\e[1A\r\e[K\e[1;32m%s\e[0m' "$typed_phrase"
-		[[ $phrase == $typed_phrase ]] && {
-			type_status='pass'
+	[[ $typed_char != . ]] && {
+		echo
+		typed_phrase+=$typed_char
+		if [[ $phrase =~ ^$typed_phrase ]] ; then
+			characters=${#typed_phrase}
+			printf '\e[1A\r\e[K\e[1;32m%s\e[0m' "$typed_phrase"
+			[[ $phrase == $typed_phrase ]] && {
+				type_status='pass'
+				break
+			}
+		else
+			type_status='fail'
+			characters=$((characters+1))
+			printf '\e[1A\e[1;32m%s\e[1;31m%s\e[0m' \
+				"${typed_phrase::-1}" "$typed_char"
 			break
-		}
-	else
-		type_status='fail'
-		characters=$((characters+1))
-		printf '\e[1A\e[1;32m%s\e[1;31m%s\e[0m' \
-			"${typed_phrase::-1}" "$typed_char"
-		break
-	fi
+		fi
+	} 
 done 2>/dev/null
 
 timeEnd=`date +%s`
