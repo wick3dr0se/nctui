7c292f9ad03044fed4ac920cf00c012d2a3e8384
wick3dr0se <wick3dr0se@protonmail.com>
2022-06-15 22:26:32 -0500

    update colors; disable line wrapping

--- typer.sh
+++ typer.sh
@@ -3,20 +3,20 @@
 . "${0%/*}/phrases.sh"
 phrase_count=${#phrase[@]}
 phrase=${phrase[`echo $((RANDOM%phrase_count))`]}
-printf '%s\n\n\e[40;1;37m%s\e[0m\n\e[1A' \
-	"Typer - Check your type speed & accuracy" "$phrase"
+printf '%s\n\n' "Typer - Check your type speed & accuracy"
+printf '\e[40;1;37m%s\e[0m\r\e[?7l' "$phrase"
 
 timeStart=`date +%s`
 
 while read -erN1 typed_char ; do
 	[[ $typed_char != . ]] && typed_phrase+=$typed_char &&
 		if [[ $phrase =~ ^$typed_phrase ]] ; then
-			characters=${#typed_phrase}
+			typed_count=${#typed_phrase}
 			printf '\e[42;1;30m%s\e[0m' "$typed_char"
 			[[ $phrase == $typed_phrase ]] && type_status='pass' && break
 		else
 			type_status='fail'
-			characters=$((characters+1))
+			typed_count=$((typed_count+1))
 			printf '\e[41;1;30m%s\e[0m' "$typed_char"
 			break
 		fi 
